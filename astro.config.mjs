c2e5b64d6532a6f064d62dc2ec5eb96aecc23386
Karamjit Kaur <karamjitkaur@Karamjits-MacBook-Pro.local>
2025-11-30 22:04:24 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
-import tailwindcss from "@tailwindcss/vite";
 // @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+  site: 'https://karamsandhu.appwrite.network/',
   integrations: [mdx(), sitemap()],
 
   vite: {
